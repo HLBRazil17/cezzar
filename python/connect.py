bfd408b00f43b76770801e66c4b1016b267b9107
HLBRazil17 <bolinhopaulino@gmail.com>
2024-10-16 01:37:52 -0300

    gerador-connectUpdate-add.htaccess

--- python/connect.py
+++ python/connect.py
@@ -1,7 +1,43 @@
 import mysql.connector
 
-conn = mysql.connector.connect(user='root', password='etec2024', host='localhost:3306', database='gerenciadorsenhas')
+# Conectar ao banco de dados
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="etec2024",
+    database="gerenciadorsenhas"
+)
+
+# Criar um cursor
 cursor = conn.cursor()
-cursor.execute("SELECT * FROM tabela")
-data = cursor.fetchall()
-print(data)
+
+# Definir o userID que deseja atualizar
+user_id = 1
+
+# Novos valores para 'codVerify' e 'twofactorSecret'
+codVerify = "10"
+twofactorSecret = "10"
+
+# Executar a consulta para atualizar os valores de 'codVerify' e 'twofactorSecret'
+cursor.execute("""
+    UPDATE users 
+    SET codVerify = ?, twofactorSecret = ?, 
+    WHERE userID = ?,
+"""())
+
+# Confirmar as alterações
+conn.commit()
+
+# Verificar se a atualização foi bem-sucedida
+cursor.execute("SELECT * FROM users WHERE userID = "(user_id))
+user = cursor.fetchone()
+
+# Exibir o resultado atualizado
+if user:
+    print("Usuário atualizado:", user)
+else:
+    print("Usuário não encontrado")
+
+# Fechar a conexão
+cursor.close()
+conn.close()
--- python/testephp.php
+++
@@ -1,9 +0,0 @@
- <?php
-require('conectar.php');
-require('login.php');
-
-$_SESSION['userID'] = $userID;
-
-echo $userID;
-
-
